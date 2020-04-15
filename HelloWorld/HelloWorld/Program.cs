fb93f1bf94acd2b257d38e4412c80dfad3b5aac3
Trevor Osborne <trevor.osborne113@gmail.com>
2020-04-15 10:50:34 -0400

    init commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("abc");
+            int n1 = 5;
+            int n2 = 2;
+            Console.WriteLine("sum = " + (n1 + n2));
+            var t1 = "Hello";
+
+            //single line comment
+
+            /*
+             * multi line
+             * comment
+             */
+
+            double d1 = 5.7;
+            double d2 = 54.2;
+            Console.WriteLine("double sum is " + (d1 + d2));
+
+            Console.WriteLine("Enter a whole number: ");
+            int n4 = int.Parse(Console.ReadLine());
+            Console.WriteLine("You entered: " + n4);
+            if (n4 > 10)
+            {
+                Console.WriteLine("Your nubmer is greater than 10");
+            }
+            else
+            {
+                Console.WriteLine("Your number is less than or equal to 10");
+            }
+
+            string choice = "y";
+            while (choice.Contains("y"))
+            {
+                Console.WriteLine("In the loop...");
+                Console.WriteLine("Continue?");
+                choice = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ch22_grade_converter/ch22_grade_converter/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ch2_grade_converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the grade converter app");
+            string choice = "y";
+
+            while (choice.Contains("y"))
+            {
+                Console.Write("Numeric Grade?: ");
+                int numericGrade = Convert.ToInt32(Console.ReadLine());
+                string letterGrade = "";
+
+                if (numericGrade >= 88)
+                {
+                    letterGrade = "A";
+                }
+                else if (numericGrade >= 80)
+                {
+                    letterGrade = "B";
+                }
+                else if (numericGrade >= 67)
+                {
+                    letterGrade = "C";
+                }
+                else if (numericGrade >= 60)
+                {
+                    letterGrade = "D";
+                }
+                else
+                {
+                    letterGrade = "F";
+                }
+                Console.WriteLine("Letter Grade: " + letterGrade);
+
+                Console.Write("\nContinue?: ");
+                choice = Console.ReadLine();
+            }
+
+            Console.WriteLine("\nGoodbye");
+        }
+    }
+}
